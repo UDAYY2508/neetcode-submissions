a58e0d54b633fd2a9b2e3dd79282bf179f176cab
udayy <udayghorpade2508@gmail.com>
2026-09-21 16:47:04 +0530

    Time: 7 ms (29.37%) | Memory: 21.7 MB (10.93%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -2,13 +2,4 @@ class Solution:
     def singleNumber(self, nums: list[int]) -> int:
         
         
-        l = 0
-        r = 1
-
-        while r<len(nums):
-            if nums[l] == nums[r]:
-                l+=1
-                nums.pop(r)
-                r=l
-            r+=1
-        return nums[l]
+        return (2*sum(set(nums)))-sum(nums)
