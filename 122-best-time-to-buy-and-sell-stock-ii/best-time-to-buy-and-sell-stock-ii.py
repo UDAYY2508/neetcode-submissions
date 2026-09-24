8b22ac54fcae2ba7bdfa592eafea7f94e6226dc3
udayy <udayghorpade2508@gmail.com>
2026-09-24 17:59:29 +0530

    Time: 7 ms (15.56%) | Memory: 20.4 MB (23.21%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,18 +1,18 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        nums = prices
+        
         max_profit=0
         w = 0
-        for r in range(1,len(nums)):
+        for r in range(1,len(prices)):
             profit=0
-            if nums[w]>nums[r]:
+            if prices[w]>prices[r]:
                 w+=1
-            elif nums[r]>nums[w]:
-                profit = nums[r]-nums[w]
+            elif prices[r]>prices[w]:
+                profit = prices[r]-prices[w]
                 max_profit+=profit
                 w+=1
                 r+=1
-            elif nums[r] == nums[w]:
+            elif prices[r] == prices[w]:
                 w+=1
                 r+=1
             r+=1
